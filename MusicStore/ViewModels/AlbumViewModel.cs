c25bb3c230d6392337e1d7f1e9c2bcdba21a76bb
g4t04o4 <repmah@gmail.com>
2023-03-31 11:39:15 +0300

    usable condition

---
+++ MusicStore/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Avalonia.Media.Imaging;
+using MusicStore.Models;
+using ReactiveUI;
+
+namespace MusicStore.ViewModels;
+
+public class AlbumViewModel : ViewModelBase
+{
+    private readonly Album _album;
+    private Bitmap? _cover;
+
+    public string Artist => _album.Artist;
+
+    public string Title => _album.Title;
+
+    public AlbumViewModel(Album album)
+    {
+        _album = album;
+    }
+
+    public Bitmap? Cover
+    {
+        get => _cover;
+        private set => this.RaiseAndSetIfChanged(ref _cover, value);
+    }
+
+    public async Task LoadCover()
+    {
+        await using (var imageStream = await _album.LoadCoverBitmapAsync())
+        {
+            Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+        }
+    }
+
+    public static async Task<IEnumerable<AlbumViewModel>> LoadCached()
+    {
+        return (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x));
+    }
+
+    public async Task SaveToDiskAsync()
+    {
+        await _album.SaveAsync();
+
+        if (Cover != null)
+        {
+            var bitmap = Cover;
+
+            await Task.Run(() =>
+            {
+                using (var fs = _album.SaveCoverBitmapStream())
+                {
+                    bitmap.Save(fs);
+                }
+            });
+        }
+    }
+}
---
+++ MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using iTunesSearch.Library.Models;
+using ReactiveUI;
+using Album = MusicStore.Models.Album;
+
+namespace MusicStore.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private bool _collectionEmpty;
+
+    public MainWindowViewModel()
+    {
+        ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+
+        BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var store = new MusicStoreViewModel();
+
+            var result = await ShowDialog.Handle(store);
+
+            if (result != null)
+            {
+                Albums.Add(result);
+
+                await result.SaveToDiskAsync();
+            }
+        });
+
+        this.WhenAnyValue(x => x.Albums.Count)
+            .Subscribe(x => CollectionEmpty = x == 0);
+
+        RxApp.MainThreadScheduler.Schedule(LoadAlbums);
+    }
+
+    private async void LoadAlbums()
+    {
+        //var albums = (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x));
+
+        var albums = await AlbumViewModel.LoadCached();
+
+        foreach (var album in albums)
+        {
+            Albums.Add(album);
+        }
+
+        // foreach (var album in Albums.ToList())
+        // {
+        //     await album.LoadCover();
+        // }
+
+        LoadCovers();
+    }
+
+    private async void LoadCovers()
+    {
+        foreach (var album in Albums.ToList())
+        {
+            await album.LoadCover();
+        }
+    }
+
+    public bool CollectionEmpty
+    {
+        get => _collectionEmpty;
+        set => this.RaiseAndSetIfChanged(ref _collectionEmpty, value);
+    }
+
+    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+
+
+    public ICommand BuyMusicCommand { get; }
+
+    public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+}
